5cc406aa67a062c45b8d97f2b3cedb006a498e8f
maximedinaux <maxime@dinaux.be>
2020-08-03 12:18:31 +0200

    200803-4

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,11 @@ const Home = () => {
   return (
     <div>
       <h2>Contact form</h2>
-      <form name="contact" method="POST" action="/success" data-netlify="true">
-        <p className="name">
-          <label htmlFor="name">Name</label>
-          <input type="text" aria-label="name" />
+      <form name="contact" method="POST" action="success" data-netlify="true">
+        <p>
+          <label htmlFor="name">
+            Your Name: <input type="text" name="name" aria-label="name" />
+          </label>
         </p>
         <p>
           <button type="submit">Send</button>
