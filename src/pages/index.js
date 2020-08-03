a0218e94ae835c648701648f36590041bab23eda
maximedinaux <maxime@dinaux.be>
2020-08-03 12:31:12 +0200

    200803-7

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,10 +17,36 @@ const Home = () => {
         </div>
         <div className="element">
           <label htmlFor="list">List Option:</label>
-          <select name="role[]">
+          <select name="list-option">
             <option value="one">One</option>
             <option value="two">Two</option>
           </select>
+        </div>
+        <div className="element">
+          <label htmlFor="checkbox">CheckBox:</label>
+
+          <div className="box">
+            <input
+              type="checkbox"
+              name="checkbox-1"
+              aria-label="checkbox-1"
+              id="checkbox-1"
+            />
+            <label htmlFor="checkbox-1" for="checkbox-1">
+              First Check Box
+            </label>
+          </div>
+          <div className="box">
+            <input
+              type="checkbox"
+              name="checkbox-2"
+              aria-label="checkbox-2"
+              id="checkbox-2"
+            />
+            <label htmlFor="checkbox-2" for="checkbox-2">
+              Second Check Box
+            </label>
+          </div>
         </div>
         <div className="element">
           <label htmlFor="msg">Message:</label>
