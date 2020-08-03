0a1fd4bf7203a10dafc5927fa68c52a502dbb75a
maximedinaux <maxime@dinaux.be>
2020-08-03 12:13:12 +0200

    200803-2
    first test mail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <div>
+      <h2>Contact form</h2>
+      <form name="contact" method="POST" action="/success">
+        <p className="name">
+          <label htmlFor="name">Name</label>
+          <input type="text" aria-label="name" />
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  )
 }
+
+export default Home
---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+const Success = () => {
+  return (
+    <div>
+      <h2>Page Success</h2>
+      <div>
+        <Link to="/">Retour au formulaire</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Success
