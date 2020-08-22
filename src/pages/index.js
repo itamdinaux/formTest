573ecfb3fc0917819531fdc1d2b74f11dce8a61c
maximedinaux <maxime@dinaux.be>
2020-08-22 16:59:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import "../css/contact.css"
 const Home = () => {
-  const [checkBoxTwo, setCheckBoxTwo] = useState(false)
-  const checkTheBoxTwo = () => {
-    setCheckBoxTwo(checkBoxTwo => !checkBoxTwo)
+  const [field1, setField1] = useState(false)
+  const checkField1 = () => {
+    setField1(field1 => !field1)
   }
 
   return (
@@ -17,93 +17,40 @@ const Home = () => {
           aria-label="contact"
         />
         <div>
-          <h2>Coordonées</h2>
-          <p>
-            <label htmlFor="name">
-              Nom :
-              <input type="text" name="name" aria-label="name" />
-            </label>
-          </p>
-          <p>
-            <label htmlFor="mail">
-              Mail :
-              <input type="mail" name="mail" aria-label="mail" />
-            </label>
-          </p>
+          <h2>Select</h2>
+          <label htmlFor="select1">
+            Select 1
+            <input
+              type="radio"
+              name="select"
+              aria-label="select1"
+              onClick={() => {
+                checkField1()
+              }}
+            />
+          </label>
+          <label htmlFor="select2">
+            Select 2
+            <input type="radio" name="select" aria-label="select2" />
+          </label>
+          <label htmlFor="select3">
+            Select 3
+            <input type="radio" name="select" aria-label="select3" />
+          </label>
         </div>
-        <div>
-          <h2>Au sujet de la demande</h2>
-          <p>
-            <label>
-              List of subject
-              <select name="Sujet">
-                <option value="sujet-1">Subject 1</option>
-                <option value="sujet-2">Subject 2</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label htmlFor="msg">
-              Message
-              <textarea aria-label="msg" name="message"></textarea>
-            </label>
-          </p>
-        </div>
-        <div>
-          <h2>Selection option</h2>
-          <p>
-            <label htmlFor="select-1">
-              <input
-                type="checkbox"
-                aria-label="select-1"
-                id="select-1"
-                name="select 1"
-                value="rps select 1"
-              />
-              select 1
-            </label>
-          </p>
-          <p>
-            <label htmlFor="select-2">
-              <input
-                type="checkbox"
-                aria-label="select-2"
-                id="select-2"
-                name="select 2"
-                value="rps select 2"
-                onClick={checkTheBoxTwo}
-              />
-              select 2
-            </label>
-          </p>
-        </div>
-        <div className={checkBoxTwo ? "open" : "closed"}>
-          <h3>element of CheckBox 2</h3>
-          <p>
-            <label htmlFor="radio-1">
-              <input
-                type="radio"
-                aria-label="radio-1"
-                id="radio-1"
-                name="select radio 1"
-                value="rps selection radio 1"
-              />
-              Selection radio 1
-            </label>
-          </p>
-          <p>
-            <label htmlFor="radio-2">
-              <input
-                type="radio"
-                aria-label="radio-2"
-                id="radio-2"
-                name="select radio 2"
-                value="rps selection radio 2"
-              />
-              Selection radio 2
-            </label>
-          </p>
-        </div>
+        {field1 ? (
+          <>
+            <div>
+              <h2>Sub 1</h2>
+              <label htmlFor="sub1">
+                SubElement 1
+                <input type="text" name="sub1" aria-label="sub1" />
+              </label>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
 
         <button type="submit">Send</button>
       </form>
